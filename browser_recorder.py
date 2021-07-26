c4777b9fe718e16181c4e6ad4e86891ec8981774
Michael <michael@perceptiveautomata.com>
2021-07-26 16:44:04 -0400

    fully functional prototype

--- browser_recorder.py
+++ browser_recorder.py
@@ -1,9 +1,16 @@
 from selenium import webdriver
 import time
+import os
+import imageio
+import re
+import glob
 
 #GLOBALS
 DASH_URL= "file:///Users/michael/dash/index.html"
-WAIT_TIME = 10
+RECORDING_TIME = 10
+IMG_DIR = "/Users/michael/browser_recorder/imgs"
+GIF_DIR = "/Users/michael/browser_recorder/gifs"
+GIF_FILENAME = "test1.gif"
 
 #Create browser object, set kind to google chrome
 browser = webdriver.Chrome()
@@ -11,8 +18,26 @@ browser = webdriver.Chrome()
 #Open to DASH
 browser.get(DASH_URL)
 
-#Wait number of seconds of WAIT_TIME
-time.sleep(WAIT_TIME)
-
-#Close browser session
+start_time = time.time()
+counter = 0
+while True:
+    current_time = time.time()
+    elapsed_time = current_time - start_time
+    counter += 1
+    browser.get_screenshot_as_file(f"{IMG_DIR}/{counter}.png")
+    if elapsed_time >= RECORDING_TIME:
+        break
 browser.close()
+
+image_files = os.listdir(IMG_DIR)
+image_files.sort(key=lambda f: int(re.sub('\D', '', f)))
+
+gif = []
+with imageio.get_writer(f"{GIF_DIR}/{GIF_FILENAME}", mode='I') as writer:
+    for file in image_files:
+        image = imageio.imread(f"{IMG_DIR}/{file}")
+        writer.append_data(image)
+
+files = glob.glob(f"{IMG_DIR}/*")
+for f in files:
+    os.remove(f)
